30891ae2bdf84ef982d98750c3f825cc1b0d4655
Hyunjin Lee <hyunjin@veluga.kr>
2025-05-06 16:15:47 +0900

    Footer 스타일 수정 및 screen-only 스타일 수정

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -2,7 +2,7 @@ import { SubInfo } from "@/components";
 
 export default function ScreenOnlyFooter() {
   return (
-    <footer className="screen-only p-12 w-auto mx-auto">
+    <footer className="screen-only p-12 w-auto mx-auto text-center">
       <SubInfo>
         이력서의 소스 코드는{" "}
         <a href="https://github.com/freejak5520/resume" target="_blank">
